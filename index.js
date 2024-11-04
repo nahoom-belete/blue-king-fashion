2777de9a7b6e88fb3887f4151e0be6a77a3a2696
Nahoom Belete <nahoombelete24@gmail.com>
2024-11-04 20:27:14 +0200

    Fix catalogue responsiveness issues

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var express = require("express");
 var app = express();
 
-app.get('/helloworld', function(req, res) {
+/* Get Routes*/
+app.get('/hello', function(req, res) {
     res.send("Hello world!");
 });
+
+
+/* Post Routes */
+app.post('/hello', function(req, res){
+    res.send("You just called the post method at '/hello'!\n");
+ });
+ 
+var routes = require('./routes.js');
+
+app.use('./routes', routes);
+
+console.log("Server started")
 
 app.listen(3000);
